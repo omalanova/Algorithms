6a5c749904447fd2c1a71ba1a35afba8cf66b619
omalanova <malanovaoe@a-333.ru>
2021-10-18 22:32:26 +0800

    lesson's exercise

---
+++ src/test/java/FizzBuzz35Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class FizzBuzz35Test {
+
+    @Test
+    public void  testFizzBuzz() {
+        int startNumber = 1;
+        int endNumber = 15;
+        String[] expectedResult = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "12", "13", "14", "FizzBuzz"};
+    }
+}

9f6306b9e76342eeaa5ea6007390f171fdea2d79
omalanova <malanovaoe@a-333.ru>
2021-11-01 13:56:13 +0800

    refactoring Algorithms

--- src/main/java/massiv/CompareArrays.java
+++ src/main/java/massiv/CompareArrays.java
@@ -1,3 +1,5 @@
+package massiv;
+
 import java.util.Arrays;
 
 public class CompareArrays {
---
+++ src/test/java/stroki/CharOddIndexTest.java
@@ -0,0 +1,20 @@
+package stroki;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CharOddIndexTest {
+    @Test
+    public void testCharOddIndex () {
+        String expectedResult1 = "AoEeyn";
+        String expectedResult2 = "aaesnAeu";
+
+        CharOddIndex CharOddIndexObj = new CharOddIndex();
+
+        String actualResult1 = CharOddIndexObj.outputCharOddIndex("QAForEveryone");
+        String actualResult2 = CharOddIndexObj.outputCharOddIndex("JavaLessonsAreFun");
+
+        Assert.assertEquals(expectedResult1, actualResult1);
+        Assert.assertEquals(expectedResult2, actualResult2);
+    }
+}
